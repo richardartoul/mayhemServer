ed004bc5c456a186fbd7055bd26133a1f73e9131
Richard Artoul <richardartoul@gmail.com>
2015-05-19 19:55:09 -0700

    Server will store all users on the same webpage in an array

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var websites = {};
+var users = {};
 
 app.get('/', function(req,res) {
 	res.send("Hello World");
@@ -10,19 +11,33 @@ app.get('/', function(req,res) {
 
 io.on('connection', function(socket) {
 	console.log("A user connected");
-	socket.on("playerUrl", function(playerUrl) {
+	socket.on("playerUrl", function(urlObj) {
 		/* its probably really inefficient to use website urls as keys to an object.
 		An easy short-term solution would be use a hasing function client side
 		to convert the urls to a simpler key, but this would result in collissions
 		eventually and its own host of issues*/  
-		if (!websites[playerUrl]) {
-			websites[playerUrl] = {};
+		if (!websites[urlObj.url]) {
+			websites[urlObj.url] = [];
 		}
+		
+		websites[urlObj.url].push(urlObj.player);
+
+		users[urlObj.url] = {
+			socket:socket,
+			url: urlObj.url,
+			shipData: undefined
+		} 
+		console.log(urlObj.url," array contains", websites[urlObj.url]);
 		// websites[playerUrl][player]
 	});
 	socket.on("playerLocation", function(playerObj) {
-		console.log(playerObj);
-
+		// console.log(playerObj);
+		//do I even need to store these?
+		playerUrl = users[playerObj.player].url;
+		users[playerObj.player].shipData = playerObj;
+		for (var i = 0; i < websites[playerUrl].length; i++) {
+			websites[playerUrl][i].socket.emit('otherPlayerLocation', playerObj);
+		}
 	});
 	socket.on('discconect', function() {
 		console.log("A user disconected");
